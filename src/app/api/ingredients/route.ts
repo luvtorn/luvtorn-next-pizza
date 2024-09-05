82ee8360c5fd11f651a3362bfda6ffbb846b85a3
luvtorn <themrkolya0123@gmail.com>
2024-09-05 12:26:31 +0200

    vso rabotaet

--- src/app/api/ingredients/route.ts
+++ src/app/api/ingredients/route.ts
@@ -2,16 +2,7 @@ import { prisma } from "../../../../prisma/prisma-client";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  try {
-    const ingredients = await prisma.ingredient.findMany();
+  const ingredients = await prisma.ingredient.findMany();
 
-    return NextResponse.json(ingredients, { status: 200 });
-  } catch (error) {
-    console.error("Error fetching ingredients:", error);
-
-    return NextResponse.json(
-      { error: "Failed to fetch ingredients" },
-      { status: 500 }
-    );
-  }
+  return NextResponse.json(ingredients);
 }
--- src/services/ingredients.ts
+++ src/services/ingredients.ts
@@ -2,11 +2,6 @@ import { Ingredient } from "@prisma/client";
 import { axiosInstance } from "./instance";
 
 export const getIngredients = async (): Promise<Ingredient[]> => {
-  try {
-    const response = await axiosInstance.get<Ingredient[]>("/api/ingredients");
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching ingredients from the API:", error);
-    throw new Error("Failed to fetch ingredients");
-  }
+  const response = await axiosInstance.get<Ingredient[]>("/ingredients");
+  return response.data;
 };
